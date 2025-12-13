e084fae5fc6ba6826d7bfd58a1be73dbadcc4ddf
Gelana3225 <gelogeti@gmail.com>
2025-12-12 23:45:47 -0800

    component_creation

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,6 @@
+@props(['active'=>false])
+
+<a  aria-current="{{ $active ? 'page':'false' }}"
+ class=" {{ $active ? 'bg-gray-950/50 text-white':'text-gray-300 hover:bg-white/5 hover:text-white' }}rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white"
+ {{ $attributes }}>
+ {{ $slot }}</a>
